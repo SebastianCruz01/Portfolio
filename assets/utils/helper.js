1e6f4a19479227edfb1097fa31bb6b38b98797c3
SebastianCruz01 <110071825+SebastianCruz01@users.noreply.github.com>
2023-03-06 15:21:55 -0500

    updated

--- assets/utils/helper.js
+++
@@ -1,9 +0,0 @@
-// Description: This file contains all the helper functions
-// that are used in the application
-// Function to validate email address
-// Return true if email is valid
-
-export function validateEmail(email) {
-    var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
